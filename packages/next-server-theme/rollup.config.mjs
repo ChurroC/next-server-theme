8b5effc6a29a4aae31d8e2470aa5012f289741d5
ChurroC <charanvenkatchandran@gmail.com>
2024-04-28 00:57:07 -0500

    uhhhh

--- packages/next-server-theme/rollup.config.mjs
+++ packages/next-server-theme/rollup.config.mjs
@@ -4,9 +4,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 import json from "@rollup/plugin-json";
-// use terser once everythign works
 
 export default [
   {
