8e9dbd82732c50a8d91aa4aac408598bf37da578
ChurroC <charanvenkatchandran@gmail.com>
2024-05-25 15:05:07 -0500

    got analytics done but could be better optimtimized

--- apps/comparison/nextServerTheme/src/app/api/analytics/route.ts
+++ apps/comparison/nextServerTheme/src/app/api/analytics/route.ts
@@ -24,24 +24,32 @@ export async function POST(request: Request) {
                 // the data when I'm writing it else redo and use new data
                 time = parsedData.find(
                     (item: any) => item.name === "time"
-                ).value;
+                )?.value;
 
                 let found = false;
+                let foundTime = false;
                 newData = parsedData.map((item: any) => {
                     if (item.name === body.name) {
                         found = true;
                         return body;
+                    } else if (item.name === "time") {
+                        foundTime = true;
+                        return { name: "time", value: Date.now() };
                     }
                     return item;
                 });
                 if (!found) {
                     newData.push(body);
                 }
+                if (!foundTime) {
+                    newData.push({ name: "time", value: Date.now() });
+                }
             } while (
-                time ===
-                parsedData.find((item: any) => item.name === "time").value
+                time !==
+                parsedData.find((item: any) => item.name === "time")?.value
             );
 
+            // write file
             await fs.mkdir("analytics", { recursive: true });
             await fs.writeFile(
                 "analytics/analytics.json",
--- apps/comparison/nextThemes/src/app/api/analytics/route.ts
+++ apps/comparison/nextThemes/src/app/api/analytics/route.ts
@@ -1,35 +1,58 @@
-/* eslint-disable */
-
 import fs from "fs/promises";
 
 // check if in production
 export async function POST(request: Request) {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.analytics || process.env.NODE_ENV === "production") {
         try {
             console.log("Analytics request received.");
             const body = await request.json();
-            let parsed;
-            try {
-                const data = await fs.readFile("analytics.json", "utf8");
-                parsed = JSON.parse(data);
-            } catch (e) {
-                parsed = [];
-            }
+            let parsedData;
+            let time;
+            let newData;
 
-            let found = false;
-            const newData = parsed.map((item: any) => {
-                if (item.name === body.name) {
-                    found = true;
-                    return body;
+            do {
+                try {
+                    const data = await fs.readFile(
+                        "analytics/analytics.json",
+                        "utf8"
+                    );
+                    parsedData = JSON.parse(data);
+                } catch (e) {
+                    parsedData = [];
                 }
-                return item;
-            });
-            if (!found) {
-                newData.push(body);
-            }
+                // make sure the time or the data initally I changed is the same as
+                // the data when I'm writing it else redo and use new data
+                time = parsedData.find(
+                    (item: any) => item.name === "time"
+                )?.value;
 
+                let found = false;
+                let foundTime = false;
+                newData = parsedData.map((item: any) => {
+                    if (item.name === body.name) {
+                        found = true;
+                        return body;
+                    } else if (item.name === "time") {
+                        foundTime = true;
+                        return { name: "time", value: Date.now() };
+                    }
+                    return item;
+                });
+                if (!found) {
+                    newData.push(body);
+                }
+                if (!foundTime) {
+                    newData.push({ name: "time", value: Date.now() });
+                }
+            } while (
+                time !==
+                parsedData.find((item: any) => item.name === "time")?.value
+            );
+
+            // write file
+            await fs.mkdir("analytics", { recursive: true });
             await fs.writeFile(
-                "analytics.json",
+                "analytics/analytics.json",
                 JSON.stringify(newData),
                 "utf8"
             );
