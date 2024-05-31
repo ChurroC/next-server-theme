bdcc9d9eccece2fc3d525d768438a6fbb7e7d51a
ChurroC <charanvenkatchandran@gmail.com>
2024-05-30 19:09:51 -0500

    made a sick publish bash script

---
+++ packages/next-server-theme/publish.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+rm -rf ./dist/tsconfig.tsbuildinfo
+mkdir tempDist
+cp ./package.json ./tempDist/package.json
+cp ./README.md ./tempDist/README.md
+cp ./LICENSE ./tempDist/LICENSE
+cp -r ./dist/* ./tempDist
+sed -i -e 's/".\/dist/"./g' ./tempDist/package.json
+cd tempDist
+npm publish
+cd ..
+rm -rf ./tempDist
